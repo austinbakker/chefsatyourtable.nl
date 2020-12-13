2bb678931a8988d8c9e1ed198fe4bb65370a03cf
austinbakker <austin@austinbakker.com>
2020-12-13 11:41:46 +0100

    extra mogelijkheden en locatie klaar

--- components/extramogelijkhedenlocatie.vue
+++ components/extramogelijkhedenlocatie.vue
@@ -1,18 +1,22 @@
 <template>
-<div>
+<div class="flex flex-col gap-24 md:gap-0" >
 
-  <div class="flex mx-auto md:ml-32 w-3/4" >
-    <img class="h-48 w-48" src="~/assets/images/extramogelijkhedenlocatie/1.png" alt="food image">
-    <div class="flex flex-col justify-center w-96" >
+  <div class="flex flex-col items-center md:flex-row mx-auto md:ml-32 md:w-3/4" >
+    <img class="w-48 object-contain" src="~/assets/images/extramogelijkhedenlocatie/1.png" alt="food image">
+    <div class="flex flex-col justify-center w-96 ml-24" >
       <h4>Extra mogelijkheden</h4>
       <p>Wilt u de avond net dat beetje extra geven? Dan is het mogelijk om een gekwalificeerde sommelier of bartender in te schakelen. Informeer gerust naar de mogelijkheden als u daar interesse in heeft.</p>
     </div>
   </div>
 
-  <div>
-    <img src="~/assets/images/extramogelijkhedenlocatie/2.png" alt="image of food 2">
-    <h4>Op locatie</h4>
-    <p>Voor een aanvraag voor grotere gezelschappen, voor speciale gelegenheden of op bijzondere plekken kan het handig zijn wanneer wij van tevoren samen met u een bezoek brengen aan de locatie. Op deze manier kunnen wij uw wensen bespreken en de keuken in kaart te brengen. </p>
+  <div class="flex flex-col md:w-full md:flex-row items-center h-96" >
+    <div class="relative flex items-center" style="height:100%;width:20rem" >
+      <img class="relative md:absolute md:-left-1/4 " style="width:100%;width:40rem;max-width:auto;" src="~/assets/images/extramogelijkhedenlocatie/2.png" alt="image of food 2">
+    </div>
+    <div class="max-w-2xl ml-10 w-96 md:w-full" >
+      <h4>Op locatie</h4>
+      <p>Voor een aanvraag voor grotere gezelschappen, voor speciale gelegenheden of op bijzondere plekken kan het handig zijn wanneer wij van tevoren samen met u een bezoek brengen aan de locatie. Op deze manier kunnen wij uw wensen bespreken en de keuken in kaart te brengen. </p>
+    </div>
   </div>
 
 </div>
